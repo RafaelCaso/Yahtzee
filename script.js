497452efac4a30d8c7632399d5ce11b8a8232b0e
RafaelCaso <94573618+RafaelCaso@users.noreply.github.com>
2022-02-10 16:19:27 -0500

    Add placeScore function to get value of dice and add it to player object. Function still does not promptUser if score box already set. Reason unclear

--- script.js
+++ script.js
@@ -23,11 +23,32 @@ class Player {
   }
 }
 
-const rollDiceEl = $(".dice");
 const rollBtn = $(".roll");
+const scoreBtn = $(".score");
 const userPrompt = $("#userPrompt");
 
 const playerOne = new Player("Chet");
+
+let hand, timesRolled;
+
+timesRolled = 0;
+
+rollBtn.click(() => {
+  rollDice();
+});
+
+scoreBtn.click(() => {
+  timesRolled = 0;
+  placeScore();
+  $(".dice").removeClass("dice--selected");
+  promptUser("Click on Roll button to begin");
+});
+
+// // console log player's upper score
+// for (const key in playerOne.score.upper) {
+//   console.log(key);
+//   console.log(playerOne.score.upper[key]);
+// }
 
 // toggle class 'dice-selected' whenever user clicks on a die
 for (let i = 1; i <= 6; i++) {
@@ -37,31 +58,68 @@ for (let i = 1; i <= 6; i++) {
   });
 }
 
-let rollAmount = 0;
-let diceToRoll = 5;
-let firstHand = [];
-
-// Rolls new number if die does NOT have class 'dice--selected'
+// Iterate through all dice and roll new number if die does NOT have class 'dice--selected' and player has not already rolled 3 times
 function rollDice() {
-  firstHand = [];
-  $(".dice").each(function () {
-    if ($(this).hasClass("dice--selected")) {
-      void 0;
-    } else {
-      let n = Math.trunc(Math.random() * 6) + 1;
-      $(this).attr("src", `images/dice-${n}.png`);
-      firstHand.push(n);
-      console.log(firstHand);
-    }
-  });
+  if (timesRolled < 3) {
+    $(".dice").each(function () {
+      if ($(this).hasClass("dice--selected")) {
+        void 0;
+      } else {
+        let n = Math.trunc(Math.random() * 6) + 1;
+        $(this).attr("src", `images/dice-${n}.png`);
+      }
+    });
+    timesRolled++;
+  }
+  if (timesRolled === 3) {
+    promptUser("No more rolls, you must place a score");
+  }
 }
 
-rollBtn.click(() => {
-  rollDice();
-});
+function promptUser(message) {
+  userPrompt.text(message);
+}
 
-function promptUser() {
-  userPrompt.text(
-    `Which dice would you like to keep, ${playerOne.playerName}?`
-  );
+function displayScoreBox() {
+  for (const key in playerOne.score.upper) {
+    $("#scoreBoxUpper").append(
+      `<input type='radio' id='upper.${key}' name='score'><p>${key}</p>`
+    );
+  }
+
+  for (const key in playerOne.score.lower) {
+    $("#scoreBoxLower").append(
+      `<input type='radio' id='lower.${key}' name='score'><p>${key}</p>`
+    );
+  }
+}
+
+function displayPlayerScore() {
+  for (const key in playerOne.score.upper) {
+    $("#scoreBoxUpper").append(`<p>${playerOne.score.upper[key]}</p>`);
+  }
+
+  for (const key in playerOne.score.lower) {
+    $("#scoreBoxLower").append(`<p>${playerOne.score.lower[key]}</p>`);
+  }
+}
+
+displayScoreBox();
+
+displayPlayerScore();
+
+function placeScore() {
+  //$(".btns-box").append("<button class='submitScore'>Submit</button>");
+  hand = [];
+  $(".dice").each(function () {
+    const score = $(this).attr("src");
+    hand.push(Number(score.split("")[12]));
+  });
+  promptUser("Select the box you would like to score then click enter");
+  let scoreBox = $("input[name=score]:checked").attr("id");
+  if (playerOne.score[scoreBox]) {
+    promptUser("Sorry, that box has already been scored");
+  } else {
+    playerOne.score[scoreBox] = hand;
+  }
 }
