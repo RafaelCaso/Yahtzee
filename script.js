3d346cd008a500e37628e0060062ed13ccde8693
RafaelCaso <94573618+RafaelCaso@users.noreply.github.com>
2022-02-18 15:26:12 -0500

    add checkGameOver function and finalScore function

--- script.js
+++ script.js
@@ -8,15 +8,10 @@ let hand, timesRolled;
 let canRoll = true;
 let isPlaying = true;
 
-if (!isPlaying) {
-  promptUser("GAME OVER");
-  canRoll = false;
-  $(".dice").attr("src", "/images/blank.png");
-}
-
 timesRolled = 0;
 
 rollBtn.click(() => {
+  checkGameOver();
   if (isPlaying) {
     if (canRoll) {
       rollDice();
@@ -25,6 +20,7 @@ rollBtn.click(() => {
 });
 
 scoreBtn.click(() => {
+  checkGameOver();
   timesRolled = 0;
   placeScore();
   $(".dice").removeClass("dice--selected");
@@ -46,6 +42,7 @@ function promptUser(message) {
 
 // Iterate through all dice and roll new number if die does NOT have class 'dice--selected' and player has not already rolled 3 times
 function rollDice() {
+  checkGameOver();
   if (canRoll) {
     $(".dice").each(function () {
       if ($(this).hasClass("dice--selected")) {
@@ -69,77 +66,76 @@ function displayScoreBox() {
   $("#scoreBoxUpper").append(
     `<input type='radio' id='0' name='score'><p class='label0'>aces</p>`
   );
-  // $("#scoreBoxUpper").append(
-  //   `<input type='radio' id='1' name='score'><p class='label1'>twos</p>`
-  // );
-  // $("#scoreBoxUpper").append(
-  //   `<input type='radio' id='2' name='score'><p class='label2'>threes</p>`
-  // );
-  // $("#scoreBoxUpper").append(
-  //   `<input type='radio' id='3' name='score'><p class='label3'>fours</p>`
-  // );
-  // $("#scoreBoxUpper").append(
-  //   `<input type='radio' id='4' name='score'><p class='label4'>fives</p>`
-  // );
-  // $("#scoreBoxUpper").append(
-  //   `<input type='radio' id='5' name='score'><p class='label5'>sixes</p>`
-  // );
-  // $("#scoreBoxLower").append(
-  //   `<input type='radio' id='6' name='score'><p class='label6'>Three of a Kind</p>`
-  // );
-  // $("#scoreBoxLower").append(
-  //   `<input type='radio' id='7' name='score'><p class='label7'>Four of a Kind</p>`
-  // );
-  // $("#scoreBoxLower").append(
-  //   `<input type='radio' id='8' name='score'><p class='label8'>Full House</p>`
-  // );
-  // $("#scoreBoxLower").append(
-  //   `<input type='radio' id='9' name='score'><p class='label9'>Small Straight</p>`
-  // );
-  // $("#scoreBoxLower").append(
-  //   `<input type='radio' id='10' name='score'><p class='label10'>Large Straight</p>`
-  // );
-  // $("#scoreBoxLower").append(
-  //   `<input type='radio' id='11' name='score'><p class='label11'>Yahtzee</p>`
-  // );
-  // $("#scoreBoxLower").append(
-  //   `<input type='radio' id='12' name='score'><p class='label12'>Chance</p>`
-  // );
+  $("#scoreBoxUpper").append(
+    `<input type='radio' id='1' name='score'><p class='label1'>twos</p>`
+  );
+  $("#scoreBoxUpper").append(
+    `<input type='radio' id='2' name='score'><p class='label2'>threes</p>`
+  );
+  $("#scoreBoxUpper").append(
+    `<input type='radio' id='3' name='score'><p class='label3'>fours</p>`
+  );
+  $("#scoreBoxUpper").append(
+    `<input type='radio' id='4' name='score'><p class='label4'>fives</p>`
+  );
+  $("#scoreBoxUpper").append(
+    `<input type='radio' id='5' name='score'><p class='label5'>sixes</p>`
+  );
+  $("#scoreBoxLower").append(
+    `<input type='radio' id='6' name='score'><p class='label6'>Three of a Kind</p>`
+  );
+  $("#scoreBoxLower").append(
+    `<input type='radio' id='7' name='score'><p class='label7'>Four of a Kind</p>`
+  );
+  $("#scoreBoxLower").append(
+    `<input type='radio' id='8' name='score'><p class='label8'>Full House</p>`
+  );
+  $("#scoreBoxLower").append(
+    `<input type='radio' id='9' name='score'><p class='label9'>Small Straight</p>`
+  );
+  $("#scoreBoxLower").append(
+    `<input type='radio' id='10' name='score'><p class='label10'>Large Straight</p>`
+  );
+  $("#scoreBoxLower").append(
+    `<input type='radio' id='11' name='score'><p class='label11'>Yahtzee</p>`
+  );
+  $("#scoreBoxLower").append(
+    `<input type='radio' id='12' name='score'><p class='label12'>Chance</p>`
+  );
 }
 
 displayScoreBox();
 
 function placeScore() {
-  hand = [];
-  // get the value for dice by extracting the number from image file name then push that number to hand
-  $(".dice").each(function () {
-    const score = $(this).attr("src");
-    hand.push(Number(score.split("")[12]));
-  });
-  // below conditionals not working as expected
-  promptUser("Select the box you would like to score then click 'Place Score'");
-  let scoreBox = $("input[name=score]:checked").attr("id");
-  if (score[scoreBox].length > 0) {
-    promptUser("That box is already scored. Try again");
-  } else {
-    score[scoreBox] = hand;
-    canRoll = true;
-    $("input[name=score]:checked").remove();
-    $(`.label${scoreBox}`).remove();
-    $(".dice").removeClass("dice--selected").attr("src", "images/blank.png");
-    if (gameOver()) {
-      isPlaying = false;
+  checkGameOver();
+  if (isPlaying) {
+    hand = [];
+    // get the value for dice by extracting the number from image file name then push that number to hand
+    $(".dice").each(function () {
+      const score = $(this).attr("src");
+      hand.push(Number(score.split("")[12]));
+    });
+    // below conditionals not working as expected
+    promptUser(
+      "Select the box you would like to score then click 'Place Score'"
+    );
+    let scoreBox = $("input[name=score]:checked").attr("id");
+    if (score[scoreBox].length > 0) {
+      promptUser("That box is already scored. Try again");
+    } else {
+      score[scoreBox] = hand;
+      canRoll = true;
+      $("input[name=score]:checked").remove();
+      $(`.label${scoreBox}`).remove();
+      $(".dice").removeClass("dice--selected").attr("src", "images/blank.png");
+      checkGameOver();
     }
+    checkGameOver();
+  } else {
+    void 0;
   }
+  checkGameOver();
 }
-
-// checks if both score boxes have children. If NOT, returns false
-const gameOver = function () {
-  return (
-    $("#scoreBoxUpper").children().length === 0 &&
-    $("#scoreBoxLower").children().length === 0
-  );
-};
 
 // upper scoring
 // check if score box contains corresponding dice value (v) and add 1 to total amount (a). Total then multiplied by corresponding scorebox (eg. 2 for 2's, 3 for 3's etc.)
@@ -161,5 +157,37 @@ const yahtzeeScore = () => {
     return 50;
   } else {
     return 0;
+  }
+};
+
+const finalScore = () => {
+  let output =
+    aceScore(score[0]) +
+    twoScore(score[1]) +
+    threeScore(score[2]) +
+    fourScore(score[3]) +
+    fiveScore(score[4]) +
+    sixScore(score[5]) +
+    yahtzeeScore();
+
+  return output;
+};
+
+// checks if player has filled in every score box and ends game if true
+const checkGameOver = function () {
+  let emptyScoreBoxes = 13;
+
+  for (let i = 0; i < score.length; i++) {
+    if (score[i] === undefined || score[i].length === 0) {
+      void 0;
+    } else {
+      emptyScoreBoxes--;
+    }
+  }
+  if (emptyScoreBoxes === 0) {
+    promptUser(`GAME OVER Your Score:${finalScore()}`);
+    isPlaying = false;
+    canRoll = false;
+    $(".dice").remove();
   }
 };
